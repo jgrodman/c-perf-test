ae51a4f69660123f1d10ea8935bd11e206989dd0
Ubuntu <ubuntu@ip-172-31-8-124.us-west-2.compute.internal>
2019-08-07 10:36:40 +0000

    int -> long

--- visit.c
+++ visit.c
@@ -5,7 +5,7 @@
 void visit(void (*visitor)()) {
   long int num_reps = 1000L * 1000 * 1000 * 1 ;
   printf("start %lu\n", num_reps);
-  for (int i = 0; i < num_reps; i++) {
+  for (long i = 0; i < num_reps; i++) {
     visitor();
   }
   printf("end\n");
